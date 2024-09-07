3f357edee6e0a9476961981255a4693f4b8df3d2
Sam Debruyn <sam@debruyn.dev>
2024-09-07 15:02:02 +0200

    add dbt version

--- models/staging/stg_supplies.sql
+++ models/staging/stg_supplies.sql
@@ -1,31 +1,31 @@
-with src as (
-    select *
-    from {{ source('ecom', 'raw_supplies') }}
+with
+
+source as (
+
+    select * from {{ source('ecom', 'raw_supplies') }}
+
 ),
 
 renamed as (
-    select
-        id as supply_id,
-        name as supply_name,
-        cost as supply_cost,
-        perishable,
-        sku
-    from src
-),
 
-final as (
     select
-        {{ dbt_utils.generate_surrogate_key(['supply_id', 'sku']) }} as supply_uuid,
-        supply_id,
-        supply_name,
-        {{ cents_to_dollars("supply_cost") }} as supply_cost,
-        case
-            when perishable = 'True' then 1
-            else 0
-        end as perishable,
-        sku
-    from renamed
+
+        ----------  ids
+        {{ dbt_utils.generate_surrogate_key(['id', 'sku']) }} as supply_uuid,
+        id as supply_id,
+        sku as product_id,
+
+        ---------- text
+        name as supply_name,
+
+        ---------- numerics
+        {{ cents_to_dollars('cost') }} as supply_cost,
+
+        ---------- booleans
+        perishable as is_perishable_supply
+
+    from source
+
 )
 
-select *
-from final
+select * from renamed
